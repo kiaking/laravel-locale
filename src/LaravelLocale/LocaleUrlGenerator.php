0e8622f1e6edc3fac30b4cd8fd59d1b73b262ec9
Kia King Ishii <kia.king.08@gmail.com>
2015-09-17 00:53:20 +0900

    Fix locale uri segment not been replaced properly

--- src/LaravelLocale/LocaleUrlGenerator.php
+++ src/LaravelLocale/LocaleUrlGenerator.php
@@ -1,124 +1,139 @@
-<?php namespace KiaKing\LaravelLocale;
+<?php
+
+namespace KiaKing\LaravelLocale;
 
 use Illuminate\Contracts\Config\Repository as Config;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Routing\UrlGenerator;
 
-class LocaleUrlGenerator {
+class LocaleUrlGenerator
+{
+    /**
+     * Instance of config.
+     *
+     * @var \Illuminate\Contracts\Config\Repository
+     */
+    protected $config;
 
-	/**
-	 * Instance of config.
-	 *
-	 * @var \Illuminate\Contracts\Config\Repository
-	 */
-	protected $config;
+    /**
+     * Instance of request.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
 
-	/**
-	 * Instance of request.
-	 *
-	 * @var \Illuminate\Http\Request
-	 */
-	protected $request;
+    /**
+     * Instance of url generator.
+     *
+     * @var \Illuminate\Contracts\Routing\UrlGenerator
+     */
+    protected $url;
 
-	/**
-	 * Instance of url generator.
-	 *
-	 * @var \Illuminate\Contracts\Routing\UrlGenerator
-	 */
-	protected $url;
+    /**
+     * Create LocaleUrlGenerator instance.
+     *
+     * @param \Illuminate\Contracts\Config\Repository $config
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\UrlGenerator $url
+     */
+    function __construct(Config $config, Request $request, UrlGenerator $url)
+    {
+        $this->config = $config;
+        $this->request = $request;
+        $this->url = $url;
+    }
 
-	/**
-	 * Create LocaleUrlGenerator instance.
-	 *
-	 * @param \Illuminate\Contracts\Config\Repository $config
-	 * @param \Illuminate\Http\Request $request
-	 * @param \Illuminate\Contracts\Routing\UrlGenerator $url
-	 */
-	function __construct(Config $config, Request $request, UrlGenerator $url)
-	{
-		$this->config = $config;
-		$this->request = $request;
-		$this->url = $url;
-	}
+    /**
+     * Generate a absolute URL to the given path.
+     *
+     * @param  string $path
+     * @param  mixed  $extra
+     * @param  bool   $secure
+     * @return string
+     */
+    public function url($path, $extra = [], $secure = null)
+    {
+        if ($this->isCurrentLocaleDefault()) {
+            return $this->url->to($path, $extra, $secure);
+        }
 
-	/**
-	 * Generate a absolute URL to the given path.
-	 *
-	 * @param  string $path
-	 * @param  mixed  $extra
-	 * @param  bool   $secure
-	 * @return string
-	 */
-	public function url($path, $extra = [], $secure = null)
-	{
-		if ($this->isCurrentLocaleDefault())
-		{
-			return $this->url->to($path, $extra, $secure);
-		}
+        $path = $this->config->get('app.locale') . '/' . $path;
 
-		$path = $this->config->get('app.locale') . '/' . $path;
+        return $this->url->to($path, $extra, $secure);
+    }
 
-		return $this->url->to($path, $extra, $secure);
-	}
+    /**
+     * Get the URL to a named route.
+     *
+     * @param  string $name
+     * @param  mixed  $parameters
+     * @param  bool   $absolute
+     * @return string
+     */
+    public function route($name, $parameters = [], $absolute = true)
+    {
+        if ($this->isCurrentLocaleDefault()) {
+            return $this->url->route($name, $parameters, $absolute);
+        }
 
-	/**
-	 * Get the URL to a named route.
-	 *
-	 * @param  string $name
-	 * @param  mixed  $parameters
-	 * @param  bool   $absolute
-	 * @return string
-	 */
-	public function route($name, $parameters = [], $absolute = true)
-	{
-		if ($this->isCurrentLocaleDefault())
-		{
-			return $this->url->route($name, $parameters, $absolute);
-		}
+        $name = $this->config->get('app.locale') . '.' . $name;
 
-		$name = $this->config->get('app.locale') . '.' . $name;
+        return $this->url->route($name, $parameters, $absolute);
+    }
 
-		return $this->url->route($name, $parameters, $absolute);
-	}
+    /**
+     * Get the URL to a different locale.
+     *
+     * @param  string $locale
+     * @return string
+     */
+    public function change($locale)
+    {
+        if ($locale == $this->config->get('app.locale')) {
+            return $this->request->fullUrl();
+        }
 
-	/**
-	 * Get the URL to a different locale.
-	 *
-	 * @param  string $locale
-	 * @return string
-	 */
-	public function change($locale)
-	{
-		if ($locale == $this->config->get('app.locale'))
-		{
-			return $this->request->fullUrl();
-		}
+        if ($locale == $this->config->get('app.fallback_locale')) {
+            return $this->replaceLocaleString('/');
+        }
 
-		$preg = '/\/.{2}($|\/)/';
+        if ( ! $this->isCurrentLocaleDefault()) {
+            return $this->replaceLocaleString("/{$locale}/");
+        }
 
-		if ($locale == $this->config->get('app.fallback_locale'))
-		{
-			return preg_replace($preg, '/', $this->request->fullUrl());
-		}
+        $root = $this->request->root();
 
-		if ( ! $this->isCurrentLocaleDefault())
-		{
-			return preg_replace($preg, "/{$locale}/", $this->request->fullUrl());
-		}
+        return str_replace($root, "{$root}/{$locale}", $this->request->fullUrl());
+    }
 
-		$root = $this->request->root();
+    /**
+     * Replace locale string from uri.
+     *
+     * @param  string $replace
+     * @return string
+     */
+    protected function replaceLocaleString($replace)
+    {
+        $preg = '';
+        $count = 0;
 
-		return str_replace($root, "{$root}/{$locale}", $this->request->fullUrl());
-	}
+        foreach ($this->config->get('locale.available_locales') as $key => $available) {
+            if ($available != $this->config->get('app.fallback_locale')) {
+                $preg = $count > 0 ? $preg.'|'.$available : $available;
+                $count++;
+            }
+        }
 
-	/**
-	 * Check if current locale is default locale or not.
-	 *
-	 * @return bool
-	 */
-	protected function isCurrentLocaleDefault()
-	{
-		return ($this->config->get('app.locale') == $this->config->get('app.fallback_locale'));
-	}
+        return preg_replace('/\/('.$preg.')($|\/)/', $replace, $this->request->fullUrl(), 1);
+    }
 
+    /**
+     * Check if current locale is default locale or not.
+     *
+     * @return bool
+     */
+    protected function isCurrentLocaleDefault()
+    {
+        return ($this->config->get('app.locale') == $this->config->get('app.fallback_locale'));
+    }
 }
